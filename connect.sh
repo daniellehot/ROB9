8333c4aa43c33f71ce83355c29f1de2758c74466
daniellehot <34506381+daniellehot@users.noreply.github.com>
2021-09-16 13:02:19 +0200

    Shell script for connecting to the master computer

---
+++ connect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# You can run this script by running the following command in your terminal - "source connect.sh"
+# DO NOT CHANGE MASTER_URI, only change ROS_IP to the IP of your computer
+export ROS_MASTER_URI="http://192.168.1.102:11311"
+export ROS_IP="192.168.1.104"
+
